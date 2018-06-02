d0ced3c2f132c8b923a43e0401d5ec3452254cb1
scott <scott@SCOTTSDEVMACH>
2018-06-02 11:28:07 -0600

    Added basic C# structure to Warrior program

---
+++ Warrior_CSharp/Characters/Warriors/Warrior.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Characters.Warriors
+{
+
+
+    public class Warrior // Warrior class...
+    {
+        // good practice to cluster variables together by type
+        private int height; // Camel case for fields   ... another one is using _height depending on your employer
+        private int weight; // Fields are designed to be used for internal use only 
+        private string name;
+
+        public int Height { get; set; } // Pascal case for properties  // Not yet linked to height...
+        public int Weight { get; set; } // Properties are mainly for external use 
+        public string Name { get; set; }
+
+
+        public Warrior(int height, int weight, string name)
+            // basic constructor  provides initial arguments to the objects at the time it is created
+        {
+            Height = height;
+            Weight = weight;
+            Name = name;
+
+        }
+
+
+
+
+
+        public void Greetings(Warrior warrior)
+            // public AM, no return value, Greetings method that takes in a "Warrior" object value
+        {
+            Console.WriteLine($@"Greetings, {warrior.Name}!");
+        }
+    }
+}
---
+++ Warrior_CSharp/EntryPoint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Characters.Warriors;
+
+namespace Warrior_CSharp
+{
+    
+
+    public class EntryPoint  // actual start of the program 
+    {
+        static void Main(string[] args)
+        {
+            Warrior theGoodGuy = new Warrior(180, 90, "Good Guy");  // instance with no parameters (empty guy)
+            //theGoodGuy.Height = 180;   // no longer needed when passing in parameters in the constructor
+            //theGoodGuy.Weight = 90;
+            //theGoodGuy.Name = "The Good Guy";
+
+            Warrior theBadGuy = new Warrior(300, 600, "Bad Guy");
+            //theBadGuy.Height = 300;  // no longer needed whe n passing in parameters in the constructor
+            //theBadGuy.Weight = 600;
+            //theBadGuy.Name = "The Bad Guy";
+            Console.WriteLine(theBadGuy.Height);
+            Console.WriteLine(theGoodGuy.Weight);
+
+
+            theGoodGuy.Greetings(theBadGuy);  // behaviors
+            theBadGuy.Greetings(theGoodGuy);  // behaviors
+
+        }
+    }
+}
